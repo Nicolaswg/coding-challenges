429c766f0eba062bee3fededabad96243e7cee30
Nicolas Gonzalez <Nigonbol123@gmail.com>
2022-05-13 11:11:55 -0400

    solution

---
+++ javascript/Remove-parentheses.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5f7c38eb54307c002a2b8cc8/train/javascript
+
+function removeParentheses(s){
+  let result = "";
+  let count = 0;
+  for (let letter of s){
+    if (letter == "(") count += 1;
+    if (count == 0) result += letter;
+    if (letter == ")") count -= 1
+  }
+  return result
+  }
